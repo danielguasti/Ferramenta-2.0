9c3c372ed7b57524a4e92ffb06334d0e4d7ccd17
daniel <mdgm1804@gmail.com>
2022-10-21 16:55:06 -0300

    primeiros avanços

--- index.js
+++ index.js
@@ -1,26 +1,48 @@
 var port, textEncoder, writableStreamClosed, writer;
+var testes = [
+  "AT???",
+  "ATSI?",
+  "ATSP?",
+  "ATCT?",
+  "ATIT?",
+  "ATPT?",
+  "ATLM?",
+  "ATSR=02",
+  "ATSR=04",
+  "ATSR?",
+  "ATLP!",
+];
 
 async function connectSerial() {
   try {
     // Escolher e conectar na porta serial escolhida
     port = await navigator.serial.requestPort();
     await port.open({ baudRate: document.getElementById("baud").value });
-    let settings = {};
 
     textEncoder = new TextEncoderStream();
     writableStreamClosed = textEncoder.readable.pipeTo(port.writable);
     writer = textEncoder.writable.getWriter();
-    await listenToPort();
+    console.log("deu certo!");
   } catch (e) {
-    alert(e);
+    console.log(e);
   }
+  await listenToPort();
 }
 
-async function sendSerialLine() {
-  dataToSend = "olá" + "\n" + "\r";
-  appendToTerminal("> " + dataToSend);
-  await writer.write(dataToSend);
-  if (dataToSend.trim().startsWith("\x03")) echo(false);
+async function sendSerialLine(dado) {
+  if (dado == testes[0]) {
+    for (var i = 0; i < 3; i++) {
+      dataToSend = testes[i] + "\n" + "\r";
+      appendToTerminal("> " + dataToSend);
+      await writer.write(dataToSend);
+      if (dataToSend.trim().startsWith("\x03")) echo(false);
+    }
+  } else {
+    dataToSend = dado + "\n" + "\r";
+    appendToTerminal("> " + dataToSend);
+    await writer.write(dataToSend);
+    if (dataToSend.trim().startsWith("\x03")) echo(false);
+  }
 }
 
 async function listenToPort() {
@@ -39,6 +61,7 @@ async function listenToPort() {
     }
     // Valor
     appendToTerminal(value);
+    console.log(value);
   }
 }
 
