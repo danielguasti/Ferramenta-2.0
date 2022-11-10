2bb548bf14a32544fadbbeaa0cacfccc95494266
daniel <mdgm1804@gmail.com>
2022-11-10 16:25:30 -0300

    Falta estilizar

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-var port, textEncoder, writableStreamClosed, writer;
+var port, textEncoder, writableStreamClosed, writer, valor, check;
 var testesSH = [];
 var testesNQ = [];
-var valor;
 
 const testessh = document.querySelector("#aparecersh");
 const SH = document.querySelector("#SH");
@@ -63,89 +62,45 @@ function verificarTestes() {
   sendSerialLine(0);
 }
 
-async function connectSerial() {
-  try {
-    // Escolher e conectar na porta serial escolhida
-    port = await navigator.serial.requestPort();
-    await port.open({ baudRate: document.getElementById("baud").value });
-
-    textEncoder = new TextEncoderStream();
-    writableStreamClosed = textEncoder.readable.pipeTo(port.writable);
-    writer = textEncoder.writable.getWriter();
-    console.log("Serial conectada!");
-  } catch (e) {
-    alert(e);
-  }
-  await listenToPort();
-}
-
 async function sendSerialLine(dado) {
   dataToSend = testesSH[dado] + "\r";
   console.log(testesSH[dado]);
   appendToTerminal(">" + dataToSend);
   await writer.write(dataToSend);
   valor = dado;
+  check = true;
 }
 
-async function listenToPort() {
-  const lineReader = port.readable
-    .pipeThrough(new TextDecoderStream())
-    .pipeThrough(new TransformStream(new LineBreakTransformer()))
-    .getReader();
-
-  // ouvir os dados vindos da Serial.
-  while (true) {
-    const { value, done } = await lineReader.read();
-    if (done) {
-      // Permite fechar a serial depois.
-      console.log("[readLoop] DONE", done);
-      reader.releaseLock();
-      break;
-    }
-    // Valor
-    appendToTerminal(value);
-  }
+async function enviarTexto() {
+  dataToSend = document.getElementById("linha-para-enviar").value + "\r";
+  console.log(dataToSend);
+  appendToTerminal(">" + dataToSend);
+  await writer.write(dataToSend);
+  check = false;
 }
 
 const serialResultsDiv = document.getElementById("serialResults");
 
 function appendToTerminal(newStuff) {
-  if (newStuff == "OK") {
-    serialResultsDiv.innerHTML += newStuff + "\n" + "\n";
-  } else {
-    serialResultsDiv.innerHTML += newStuff + "\n";
-  }
-  if (valor < selecionados - 1 && newStuff == "OK") {
-    valor++;
-    sendSerialLine(valor);
-  } else if (valor == selecionados - 1 && newStuff == "OK") {
-    testesSH = [];
-  }
-}
-
-function sleep(milliseconds) {
-  let timeStart = new Date().getTime();
-  while (true) {
-    let elapsedTime = new Date().getTime() - timeStart;
-    if (elapsedTime > milliseconds) {
-      break;
+  if (check == true) {
+    if (newStuff == "OK") {
+      serialResultsDiv.innerHTML += newStuff + "\n" + "\n";
+    } else {
+      serialResultsDiv.innerHTML += newStuff + "\n";
+    }
+    if (valor < selecionados - 1 && newStuff == "OK") {
+      valor++;
+      sendSerialLine(valor);
+    } else if (valor == selecionados - 1 && newStuff == "OK") {
+      testesSH = [];
     }
   }
-}
 
-class LineBreakTransformer {
-  constructor() {
-    this.container = "";
-  }
-
-  transform(chunk, controller) {
-    this.container += chunk;
-    const lines = this.container.split("\r\n");
-    this.container = lines.pop();
-    lines.forEach((line) => controller.enqueue(line));
-  }
-
-  flush(controller) {
-    controller.enqueue(this.container);
+  if (check == false) {
+    if (newStuff == "OK") {
+      serialResultsDiv.innerHTML += newStuff + "\n" + "\n";
+    } else {
+      serialResultsDiv.innerHTML += newStuff + "\n";
+    }
   }
 }
